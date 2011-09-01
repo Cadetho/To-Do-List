669128318aa96b64790e1685b3e0e3f26671269e
Cade J. Thomasson <cadetho@gmail.com>
2011-09-01 02:33:14 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todolist::Application.config.secret_token = '9e4c18f6ce4d87c1c72d734d8452723f1c2cb6ddf1a8576ae1d90b52d74c18218f59df8713c24798231bfb04ce4870d5908d91c9921a9205a8651d25a729b5ff'
---
+++ db/migrate/20110901083046_create_lists.rb
@@ -0,0 +1,15 @@
+class CreateLists < ActiveRecord::Migration
+  def self.up
+    create_table :lists do |t|
+      t.string :Task
+      t.text :Comments
+      t.datetime :Date_added
+      t.datetime :Finish_by
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :lists
+  end
+end
